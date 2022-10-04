b5b4756784978408723b224aa0f6a0a8406095c4
*Yuliya <yuliya.klimovich@icloud.com>
2022-10-04 22:56:28 +0300

    Initial commit

---
+++ src/main/java/XMLHandler.java
@@ -0,0 +1,40 @@
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+import java.text.SimpleDateFormat;
+import java.util.HashMap;
+
+public class XMLHandler extends DefaultHandler {
+    private Voter voter;
+    private static SimpleDateFormat birthDayFormat = new SimpleDateFormat("yyyy.MM.dd");
+    private HashMap<Voter, Integer> voterCounts;
+    public XMLHandler(){
+        voterCounts = new HashMap<>();
+    }
+    @Override
+    public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+
+        if (qName.equals("voter") && voter == null) {
+            String birthDay = attributes.getValue("birthDay");
+            voter = new Voter(attributes.getValue("name"), birthDay);
+        } else if (qName.equals("visit") && voter != null) {
+            int count = voterCounts.getOrDefault(voter, 0);
+            voterCounts.put(voter, count + 1);
+        }
+    }
+
+    @Override
+    public void endElement(String uri, String localName, String qName) throws SAXException {
+        if (qName.equals("voter")){
+            voter = null;
+        }
+    }
+    public void duplicatedVoters(){
+        for (Voter voter : voterCounts.keySet()){
+            int count = voterCounts.get(voter);
+            if(count > 1){
+                System.out.println(voter.toString() + " - " +  count);
+            }
+        }
+    }
+}
